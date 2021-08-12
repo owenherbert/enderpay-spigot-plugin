effc38458e1e366456cf5c6b99e45128c8db0676
owen-master <60369890+owen-master@users.noreply.github.com>
2021-08-12 20:33:08 +1000

    Remove unused consts

--- src/main/java/com/enderpay/config/Config.java
+++ src/main/java/com/enderpay/config/Config.java
@@ -4,7 +4,5 @@ public class Config {
 
     public static String API_KEY = "api-key";
     public static String API_SECRET = "api-secret";
-    public static String BUY_COMMAND = "buy-command";
-    public static String CLICK_SOUND = "click-sound";
 
 }
